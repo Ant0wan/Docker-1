a645470d312e58285915d38cb60facd2f7089ebf
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-21 11:32:59 -0500

    feat(make it ready for eval): put everything to smoothly work for the evaluation at 42

--- 01_dockerfiles/ex02/test.sh
+++ 01_dockerfiles/ex02/test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cd ft-rails && docker build -t ft-rails:on-build .
-cd .. && docker build -t myapp:0 .
+git clone --recurse-submodules https://github.com/gothinkster/rails-realworld-example-app.git ft-rails/app
+#cd ft-rails && docker build -t ft-rails:on-build .
+#cd .. && docker build -t myapp:0 .
 #docker run --rm -p 3000:3000 myapp:0 ex02
