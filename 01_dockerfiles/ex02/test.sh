6b20fcd3319163531d034c52680f45a46bc7971b
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-23 20:20:48 +0100

    feat(script rails): fix script to build the app

--- 01_dockerfiles/ex02/test.sh
+++ 01_dockerfiles/ex02/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Build the ruby on rails app
-docker run --rm -it -p 3000:3000 -v $PWD/ft-rails/:/opt/app ruby "/bin/sh /opt/app/build_app.sh"
+docker run --rm -it -p 3000:3000 -v $PWD/ft-rails/:/opt/app ruby "/bin/sh" "/opt/app/build_app.sh"
 
 #cd ft-rails && docker build -t ft-rails:on-build .
 #cd .. && docker build -t myapp:0 .
