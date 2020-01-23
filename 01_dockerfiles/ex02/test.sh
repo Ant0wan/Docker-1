9e0f59d9bcf99b9d51ea41ad952298ff786427a7
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-23 20:32:54 +0100

    fix(test): ruby on rails...

--- 01_dockerfiles/ex02/test.sh
+++ 01_dockerfiles/ex02/test.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-#Build the ruby on rails app
+# Build the ruby on rails app
 docker run --rm -it -p 3000:3000 -v $PWD/ft-rails/:/opt/app ruby "/bin/sh" "/opt/app/build_app.sh"
 
 #cd ft-rails && docker build -t ft-rails:on-build .
 #cd .. && docker build -t myapp:0 .
 #docker run --rm -p 3000:3000 myapp:0 ex02
+
+# Delete the app
+#rm -rf ft-rails/app
