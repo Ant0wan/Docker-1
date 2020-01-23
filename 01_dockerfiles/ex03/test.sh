fc7199ae49003b7335c4c8285a0e170a49ca5ae9
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-23 10:46:28 +0100

    feat(test gitlab): test for gitlab

--- 01_dockerfiles/ex03/test.sh
+++ 01_dockerfiles/ex03/test.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 #mkdir gitlab
 #cd gitlab && mkdir config && mkdir logs && mkdir data
-docker run --detach --rm \
-  --hostname gitlab.example.com \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
-  --name gitlab \
-  --restart always \
-  --volume ./gitlab/config:/etc/gitlab \
-  --volume ./gitlab/logs:/var/log/gitlab \
-  --volume ./gitlab/data:/var/opt/gitlab \
-  gitlab/gitlab-ce:latest
+#docker run --detach --rm \
+#  --hostname gitlab.example.com \
+#  --publish 443:443 --publish 80:80 --publish 22:22 \
+#  --name gitlab \
+#  --restart always \
+#  --volume ./gitlab/config:/etc/gitlab \
+#  --volume ./gitlab/logs:/var/log/gitlab \
+#  --volume ./gitlab/data:/var/opt/gitlab \
+#  gitlab/gitlab-ce:latest
+# --privileged
+
+docker run --rm --privileged gl:0 .
