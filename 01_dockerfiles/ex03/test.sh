df1451f14c88cdc45311de65dd4a729a229b1ced
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-24 16:31:50 +0100

    tests(doc): adding logo

--- 01_dockerfiles/ex03/test.sh
+++ 01_dockerfiles/ex03/test.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+#sudo docker run --detach \
+#  --hostname gitlab.example.com \
+#  --publish 443:443 --publish 80:80 --publish 22:22 \
+#  --name gitlab \
+#  --restart always \
+#  --volume $PWD/gitlab/config:/etc/gitlab \
+#  --volume $PWD/gitlab/logs:/var/log/gitlab \
+#  --volume $PWD/gitlab/data:/var/opt/gitlab \
+#  gitlab/gitlab-ce:latest
+
+docker build -t mygitlab:0 .
+
 # Container to build the Dockerfile
 #docker run --rm -it --name gitlab --privileged ubuntu:bionic
-docker build -t mygitlab:0 .
-docker run --rm -it --privileged --name mygitlab mygitlab:0
+#docker run --rm -it --privileged --name mygitlab mygitlab:0
