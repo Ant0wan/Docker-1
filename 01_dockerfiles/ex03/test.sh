a98ab1c7c6f0c7f832e4a9e8c488a483caac9bd2
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-24 18:14:25 +0100

    fix(gitlab): all is well

--- 01_dockerfiles/ex03/test.sh
+++ 01_dockerfiles/ex03/test.sh
@@ -3,7 +3,7 @@
 docker build -t mygitlab:0 .
 docker run -it \
   --privileged \
-  --hostname gitlab.example.com \
+  --hostname 127.0.0.2 \
   --publish 443:443 --publish 80:80 --publish 22:22 \
   --name mygitlab \
   --restart always \
@@ -11,3 +11,5 @@ docker run -it \
   --volume $PWD/gitlab/logs:/var/log/gitlab \
   --volume $PWD/gitlab/data:/var/opt/gitlab \
   mygitlab:0
+#ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+#  --hostname gitlab.example.com \
