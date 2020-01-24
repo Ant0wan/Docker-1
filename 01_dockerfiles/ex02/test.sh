587de9906339743d6ab4450a8517790509ded1af
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-24 12:37:05 +0100

    fix(ex02): rails app running

--- 01_dockerfiles/ex02/test.sh
+++ 01_dockerfiles/ex02/test.sh
@@ -2,9 +2,9 @@
 # Build the ruby on rails app
 docker run --rm -it -p 3000:3000 -v $PWD/ft-rails/:/opt/app ruby "/bin/sh" "/opt/app/build_app.sh"
 
-#cd ft-rails && docker build -t ft-rails:on-build .
-#cd .. && docker build -t myapp:0 .
-#docker run --rm -p 3000:3000 myapp:0 ex02
+# Build the ft-rails:on-build image
+cd ft-rails && docker build -t ft-rails:on-build . && cd ..
 
-# Delete the app
+docker build -t myapp:0 .
+docker run --rm -p 3000:3000 myapp:0
 #rm -rf ft-rails/app
