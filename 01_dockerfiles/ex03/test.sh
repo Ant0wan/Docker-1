fb7d91a3d16901809b48fdc9395e193c89df5ce6
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-24 16:04:14 +0100

    feat(gitlab): tests on the go

--- 01_dockerfiles/ex03/test.sh
+++ 01_dockerfiles/ex03/test.sh
@@ -3,19 +3,22 @@
 # Container to build the Dockerfile
 #docker run --rm -it --name gitlab --privileged ubuntu:bionic
 
-# Build image and prep directory
-#mkdir gitlab
-#cd gitlab && mkdir config && mkdir logs && mkdir data
-docker build -t gl:0 .
-docker run --rm -it --privileged --name gl:0
+# Prep directory
+mkdir gitlab
+cd gitlab && mkdir config && mkdir logs && mkdir data
 
-#docker run --detach --rm \
-#  --privileged \
+# Build image
+docker build -t mygitlab:0 .
+
+# Run gitlab container
+#docker run --rm --detach \
+docker run --rm -it \
+  --privileged \
+  --name mygitlab \
+  --volume $PWD/gitlab/config:/etc/gitlab \
+  --volume $PWD/gitlab/logs:/var/log/gitlab \
+  --volume $PWD/gitlab/data:/var/opt/gitlab \
+  mygitlab:0
+
 #  --hostname gitlab.example.com \
 #  --publish 443:443 --publish 80:80 --publish 22:22 \
-#  --name gitlab \
-#  --restart always \
-#  --volume ./gitlab/config:/etc/gitlab \
-#  --volume ./gitlab/logs:/var/log/gitlab \
-#  --volume ./gitlab/data:/var/opt/gitlab \
-#  gitlab/gitlab-ce:latest
