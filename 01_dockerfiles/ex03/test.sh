b9f3f9d3226371dfc69d10c5788fbf303d907c33
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-24 17:04:33 +0100

    feat(gitlab): tests on the go

--- 01_dockerfiles/ex03/test.sh
+++ 01_dockerfiles/ex03/test.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-docker build -t mygitlab:0 .
-
-# Container to build the Dockerfile
 #docker run --rm -it --name gitlab --privileged ubuntu:bionic
-#docker run --rm -it --privileged --name mygitlab mygitlab:0
-
-sudo docker run --detach \
+docker build -t mygitlab:0 .
+docker run --detach \
+  --privileged \
   --hostname gitlab.example.com \
   --publish 443:443 --publish 80:80 --publish 22:22 \
-  --name gitlab \
+  --name mygitlab \
   --restart always \
   --volume $PWD/gitlab/config:/etc/gitlab \
   --volume $PWD/gitlab/logs:/var/log/gitlab \
