af2fabb5d93f89f2c7fe6371c343e9c2e9e0d9de
Antoine Barthelemy <abarthel@student.42.fr>
2020-01-24 18:19:14 +0100

    feat(eval prep): fix all issues related to the subject

--- 01_dockerfiles/ex00/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -t myvim:0 .
-docker run -it --rm -v $PWD:/root/vim myvim:0 hello-world.c
--- 01_dockerfiles/ex01/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -t myts:0 .
-docker run --rm -p 9987:9987/udp -p 10011:10011 -p 30033:30033 myts:0 serveradmin_password=Kerrigan
